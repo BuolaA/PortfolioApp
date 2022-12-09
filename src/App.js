da2f486f75d377717760f90be09bb95c70619e1e
BuolaA <113210923+BuolaA@users.noreply.github.com>
2022-12-09 11:28:59 -0700

    Components
    header and cards seperated into components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import WorkExperience from './WorkExperience';
+import Education from './Education';
+import Technology from './Technology';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <body>
+    <Header />
+        <main class="container">
+            <div class="row mt-3">
+                <div class="col-md">
+                    <div class="card">
+                        <WorkExperience />
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="card">
+                        <Education />
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-md mb-5">
+                    <div class="card">
+                        <Technology />
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
+</div>
   );
 }
 
